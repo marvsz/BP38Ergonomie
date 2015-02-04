a1c4052d8af8c4d40fb954090b3a7ca3357f092d
Marius Gassen <marius.gassen@gmail.com>
2015-02-04 14:39:19 +0100

    RotationGroupListView added

---
+++ view/rotationGroupView/rotationgrouplistview.h
@@ -0,0 +1,17 @@
+#ifndef ROTATIONGROUPLISTVIEW_H
+#define ROTATIONGROUPLISTVIEW_H
+#include "../view/navigation/simplenavigateablewidget.h"
+
+class RotationGroupListView : public SimpleNavigateableWidget
+{
+    Q_OBJECT
+public:
+    explicit RotationGroupListView(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // ROTATIONGROUPLISTVIEW_H
